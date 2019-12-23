27837afd13a14923f3a9ce98caae7849b3fb200f
vehernan <hveneranda@yahoo.com>
2019-12-22 17:46:40 -0800

    this one works

--- server.go
+++ server.go
@@ -1,117 +1,140 @@
-package main //the executable 
+package main
 
-// impoting libraries
 import (
-	"fmt" //package in go for print stdout
-	"net/http" //web server!
-	"strings" //use str
-	"log"
 	"bufio"
-	"io/ioutil" // implements i/o utility functions
+	"io"
+	"log"
+	"net"
+	"fmt"
+	"time"
 )
 
-type ChatServer struct //
-{
+type User struct {
+	Name string
+	Output chan Message
+}
+
+type Message struct {
+	Username string
+	Text string
+}
+
+/*Room of a chatserver*/
+type ChatServer struct {
 	Users map[string]User
-	Join chan User //to know when a user has joined
-	Leave chan User //to know when user had left
-	Input chan string //what has been writen 
+	Join chan User
+	Leave chan User
+	Input chan Message
 }
 
-type User struct
-{
-	Name string // name of user 
-	Output chan Message //channel of message
-}
+/* how would we send the message?
+ * Run method: all the logic of the chat
+ * Message in the chat + action in the map of users
+ */
+func (cs *ChatServer) Run() {
+	for {
+		select {
+			case user := <-cs.Join:
+				cs.Users[user.Name] = user
+				go func() {
+					cs.Input <- Message{
+						Username: "SYSTEM",
+						Text: fmt.Sprintf("%s joined", user.Name),
+					}
+				}()
+				
+			case user := <-cs.Leave:
+				delete(cs.Users, user.Name)
+				go func() {
+					cs.Input <- Message{
+						Username: "SYSTEM",
+						Text: fmt.Sprintf("%s left", user.Name),
+					}
+				}()
 
-type Message struct
-{
-	Username string //who wrote it
-	Text string //what was put
-}
-
-func (cs *ChatServer) Run() //logic needed for process
-(
-	for (
-		select (
-		case user := <-cd.Join:
-			cs.User[user.Name] = user
-			// say "someoned joined"
-			go func() { //function that is running concurrently w/the for loop for thr mssg
-			cs.Input <- Message{
-				UserName: "SYSTEM", //itll tell me thats my message
-				Text: fmt.Sprintf("%s joined", user.Name), 
-			}
-		}()
-		case user := <-cs.Leave:
-			delete(cs.Users, user.Name)
-			//say "someonened left"
-			go func() { //function that is running concurrently w/the for loop for thr mssg
-				cs.Input <- Message{
-					UserName: "SYSTEM",
-					Text: fmt.Sprintf("%s left", user.Name),
+			case msg := <-cs.Input:
+				for _, user := range cs.Users {
+					select {
+						case user.Output <- msg:
+						case <-time.After(time.Second * 10):
+					}
+					
 				}
-			}()
-		case msg := <-cs.Input:
-			for _, u := range cs.Users { //loop through every user and send a message to that user 
-				user.Output <- msg // add channel to user called output
-			}
+		}
+	}
+}
 
-		)
-	)
-)
-
-func handleConn(chatServer *ChatServer, conn net.Conn) {
+/*****/
+func handleConn (chatServer *ChatServer, conn net.Conn) {
 	defer conn.Close()
 
-	io.WriteString(conn, "Enter your Username:")
-	
+	io.WriteString(conn, "Enter your username: ")
 	scanner := bufio.NewScanner(conn)
 	scanner.Scan()
+
 	user := User{
 		Name: scanner.Text(),
 		Output: make(chan Message),
 	}
+
 	chatServer.Join <- user
+
 	defer func() {
 		chatServer.Leave <- user
 	}()
 
-	//read from connection 
+	//read from connection
 	go func() {
 		for scanner.Scan() {
 			ln := scanner.Text()
 			chatServer.Input <- Message{user.Name, ln}
-		}
 
+		}
 	}()
-	//write to connection
+
+	//write to connection, when it is disconnects, this would return an error
 	for msg := range user.Output {
-		io.WriteString(conn, msg.Username+ ": " +msg.Text)
+		_, err := io.WriteString(conn, msg.Username+": "+msg.Text+"\n")
+		if err != nil {
+			break
+		}
 	}
 
-func main()
-{
-	server, er := net.Listen("tcp, 9000") ://error hand, listening to connection
-	//tcp is terminal, can be changed to http
-	if err := nil {
+}
+
+/* 
+ * creating the listener
+ * defer: execute a task at the end of the enclosing function (main)
+ */
+
+func main() {
+	server, err := net.Listen("tcp", ":9000")
+	if err != nil {
 		log.Fatalln(err.Error())
 	}
+
 	defer server.Close()
 
-	chatServer := &ChatServer{ //intitializing it
+	chatServer := &ChatServer{
 		Users: make(map[string]User),
-		Join: make(chan User), //to know when a user has joined
-		Leave: make(chan User), //to know when user had left
-		Input: make(chan Message), //what has been writen
+		Join: make(chan User),
+		Leave: make(chan User),
+		Input: make(chan Message),
 	}
-	go chat.Server.Run()
+
+	go chatServer.Run()
+
 
 	for {
 		conn, err := server.Accept()
-		if err := nil {
+		if err != nil {
 			log.Fatalln(err.Error())
 		}
 		go handleConn(chatServer, conn)
 	}
 }
+
+/*
+goroutine: lightweight thread of execution.
+channel: pipes that connect concurrent goroutines.
+*/
